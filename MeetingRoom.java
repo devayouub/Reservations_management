01eda3543f64fd9ed583b066e59e9dba2d827c71
devayouub <ayoubmedjahed1@gmail.com>
2025-03-21 23:02:12 +0100

    made small changes

--- MeetingRoom.java
+++ MeetingRoom.java
@@ -3,6 +3,9 @@ public class MeetingRoom {
     private String name;
     private boolean isEmpty;
 	public MeetingRoom(String name) {
+		if(!name.matches("^[A-D]\\d{2}$")) {
+		  throw new IllegalArgumentException("no available room with that name");	
+		}
 		this.name = name;
 		this.isEmpty = true;
 	}
@@ -10,6 +13,9 @@ public class MeetingRoom {
 		return name;
 	}
 	public void setName(String name) {
+		if(!name.matches("^[A-D]\\d{2}$")) {
+			  throw new IllegalArgumentException("no available room with that name");	
+			}
 		this.name = name;
 	}
 	public boolean isEmpty() {
@@ -18,5 +24,4 @@ public class MeetingRoom {
 	public void setEmpty() {
 		this.isEmpty = true;
 	}
-
 }
