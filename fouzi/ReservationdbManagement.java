70c911341d73c174309345b8964a17e67db9c827
Fouzi <fawzimarouf2005@gmail.com>
2025-03-28 01:17:39 +0100

    Update ReservationdbManagement.java

--- fouzi/ReservationdbManagement.java
+++ fouzi/ReservationdbManagement.java
@@ -1,5 +1,22 @@
 package fouzi;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+
+
 public class ReservationdbManagement {
+	
+    private static final String URL = "jdbc:mysql://localhost:3306/";
+    private static final String USER = "root";
+    private static final String DB_PASSWORD = "";
+	
+	public static Connection getConnection()throws SQLException{
+		
+		return DriverManager.getConnection(URL,USER,DB_PASSWORD);
+		
+		
+	}
 
 }
